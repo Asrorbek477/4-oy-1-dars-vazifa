bb8f7d4b7294608c2543581e238b9b123db606b8
Asrorbek477 <asrorbeksaidmurodov2004@gmail.com>
2023-12-07 21:28:06 +0500

    new 2 fail

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-let num1 = +prompt("birinchi sonni kiriting");
-let num2 = +prompt("ikkinchi sonni kiriting");
-let num3 = +prompt("uchinchi sonni kiriting");
-console.log (num1 + num2 + num3);
+// 2.Foydalanuvchidan 2 ta son kiritishini so'rang va ushbu sonlar kvadratlarining yig'indisini toping
+
+// Prompt the user to enter the first number
+let firstNumber = prompt("Enter the first number:");
+
+// Prompt the user to enter the second number
+let secondNumber = prompt("Enter the second number:");
+
+// Calculate the sum of the squares
+let sumOfSquares = Math.pow(firstNumber, 2) + Math.pow(secondNumber, 2);
+
+// Display the result
+console.log("The sum of the squares is: " + sumOfSquares);
+
+
